0cbb8b8aad061c00e0ffafdef4f848f67ff7e50b
ejanc <ejanc@EJANCKILA>
2025-11-03 15:18:52 -0600

    Add project files.

---
+++ SeamlessBackEndProject/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SeamlessBackEndProject")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SeamlessBackEndProject.Pages")]
